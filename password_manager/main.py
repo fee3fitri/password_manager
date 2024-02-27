625f0340c63e537461c8923994f15d812c345224
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-27 14:14:00 -0800

    Initialize password manager project

---
+++ password_manager/main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+# Create Tkinter window
+window = Tk()
+window.title("Password Generator")
+
+# Image Canvas
