07cf04a2df80e424b4be487d9cbdea9eaa9b4296
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-27 14:22:32 -0800

    Add image canvas to the window

--- password_manager/main.py
+++ password_manager/main.py
@@ -7,6 +7,14 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 # Create Tkinter window
 window = Tk()
-window.title("Password Generator")
+window.title("Password Manager")
+window.config(padx=100, pady=50)
 
 # Image Canvas
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(row=1, column=1)
+
+
+window.mainloop()
