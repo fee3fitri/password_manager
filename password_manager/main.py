1acba2d315954a650e87118496962623356332c8
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-27 16:08:02 -0800

    Add credentials functionality

--- password_manager/main.py
+++ password_manager/main.py
@@ -2,13 +2,22 @@ from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+
+    with open("password_manager.txt", mode="a") as file:
+        file.write(f"\n{website} | {email} | {password}")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Create Tkinter window
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=30)
 
 # Image Canvas
 canvas = Canvas(width=200, height=200, highlightthickness=0)
@@ -20,27 +29,31 @@ canvas.grid(row=1, column=1)
 website_label = Label(text="Website: ")
 website_label.grid(row=10, column=0)
 website_label.config(padx=10, pady=10)
-website_input = Entry(width=50)
+website_input = Entry(width=45)
+website_input.focus()
 website_input.grid(row=10, column=1, columnspan=2)
 
 # Email input
 email_label = Label(text="Email/username: ")
 email_label.grid(row=11, column=0)
 email_label.config(padx=10, pady=10)
-email_input = Entry(width=50)
+email_input = Entry(width=45)
+email_input.focus()
+email_input.insert(0, "safitri.shelton@gmail.com")
 email_input.grid(row=11, column=1, columnspan=2)
 
 # Password input
 password_label = Label(text="Password: ")
 password_label.grid(row=12, column=0)
 password_label.config(padx=10, pady=10)
-password_input = Entry(width=30)
+password_input = Entry(width=27)
+password_input.focus()
 password_input.grid(row=12, column=1)
 password_btn = Button(text="Generate Password")
 password_btn.grid(row=12, column=2)
 
 # Add button
-add_btn = Button(text="Add", width=50)
+add_btn = Button(text="Add", width=45, command=save_password)
 add_btn.grid(row=13, column=1, columnspan=3)
 
 window.mainloop()
